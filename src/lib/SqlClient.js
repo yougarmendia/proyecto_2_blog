9d39a4330822b522065226f0333bd4cb9c5c3937
You Garmendia <you.garmendia.ayala@ciisa.cl>
2022-04-13 12:19:33 -0400

    Resolví el porqué del issue de la db

--- src/lib/SqlClient.js
+++ src/lib/SqlClient.js
@@ -2,6 +2,8 @@ const mysql = require('mysql2/promise')
 
 class SqlClient {
   constructor () {
+    // Pool de conexiones, evita el ataque DDoS porque limita la cantidad de conexiones.
+    // Es un número limitado de conexiones ya creadas a la db que pueden reutilizadas por distintas peticiones
     this.pool = mysql.createPool({ uri: process.env.DATABASE_URL })
     this.query = this.query.bind(this) // Código basura necesario
   }
